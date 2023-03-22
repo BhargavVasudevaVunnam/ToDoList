01888e123396d1f7ea1798cab9def7c2633cc99b
BhargavVasudevaVunnam <vvbhargavchowdary@gmail.com>
2023-03-22 18:00:17 -0400

    UseReduceHook

--- src/App.js
+++ src/App.js
@@ -1,51 +1,25 @@
 import './App.css';
-import { useRef, useReducer } from 'react';
-
-
+import { useMemo,useCallback,useState,useEffect } from 'react';
 
 function App() {
-  // const [todo,settodo]=useState([]);
-  // const referencer = useRef(null);
-  // const lists=todo.map((val,counter)=>{return <li id={counter}>{val}</li>});
-  // function addtolist(input){
-  //   let copy=[...todo,input];
-  //   settodo(copy);
-  //   }
-  //   function deletefromlist(){
-  //   let copy=[...todo];
-  //   settodo(copy.slice(0,-1));
-  //   }
-
-  function reducer(state,action){
-    function function1(inputvalue){
-      let val=[...state,inputvalue];
-      return val;
-    }
-    function function2(){
-      let copy=[...state];
-      return copy.slice(0,-1);
-    }
-    switch(action.type){
-      case 'increment':
-      {
-      return function1(action.payload);
-      break;
-      }
-      case 'decrement':
-      return function2();
-      break;
-    }
-
-  }
-  const [state, dispatch] = useReducer(reducer, []);
-  const referencer=useRef([]);
-  return <div id="container">
-    <input type='text' ref={referencer}></input>
-    <button onClick={()=>dispatch({type:'increment', payload: (referencer.current.value)})}>Add</button>
-    <button onClick={()=>dispatch({type:'decrement'})}>Delete</button>
-    <ul>
-        {state.map((value,count)=>{return <li id={count}>{value}</li>})}
-    </ul>
+  const [counter,changecounter]=useState(1);
+  const [background,changebackground]=useState('black');
+  const object=useMemo(() =>{return {color: '{background}'}}, [background])
+  // {color: '{background}'};
+  useEffect(() => {
+    console.log("triggered state")
+  }, [object])
+  
+  const slowfunction=(x)=>{console.log('entering slow');for(let i=0;i<100000000000;i++){}return x*2;}
+  let changing=2;
+  
+  const out=useMemo(()=>slowfunction(changing),[changing]);
+  console.log(out);
+  return <div>
+    <button onClick={()=>changecounter((x)=>{return x+1;})}>Add</button>
+    <div><p>{counter}</p></div>
+    <div style={{backgroundColor: object.color, width: '100px', height:'100px'}}>Background</div>
+    <button onClick={()=>{changebackground((x)=>{console.log("entered"); if(x==='black'){return 'white'}else{return 'black'}})}}>change</button>
   </div>
 }
 
